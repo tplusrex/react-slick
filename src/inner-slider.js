9800512d0fb69d6260fdf20f221c8f4a8109d23c
Tony Vongprachanh <tvongpr@gmail.com>
2017-06-04 13:20:55 -0700

    Create a new setting that allows you to specify slides to be loaded

--- src/inner-slider.js
+++ src/inner-slider.js
@@ -134,6 +134,7 @@ export var InnerSlider = React.createClass({
       focusOnSelect: this.props.focusOnSelect ? this.selectHandler : null,
       currentSlide: this.state.currentSlide,
       lazyLoad: this.props.lazyLoad,
+      lazyLoadList: this.props.lazyLoadList,
       lazyLoadedList: this.state.lazyLoadedList,
       rtl: this.props.rtl,
       slideWidth: this.state.slideWidth,
